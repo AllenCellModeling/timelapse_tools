f9f79a6595e8e98c89cb02a0056ce1422a2a8c28
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-11-13 08:45:15 -0800

    Resolves [gh-1], add label generation to each frame

--- timelapse_tools/movies.py
+++ timelapse_tools/movies.py
@@ -8,6 +8,7 @@ from typing import Callable, Dict, Optional, Union
 import imageio
 import numpy as np
 from aicspylibczi import CziFile
+from lxml.etree import _Element
 from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
 
@@ -22,14 +23,17 @@ log = logging.getLogger(__name__)
 
 def _process_timepoint(
     img: CziFile,
+    metadata: _Element,
     T: int,
     S: int,
     C: Optional[int] = None,
-    norm_func: Callable = projections.im2proj,
-    norm_kwargs: Dict = {},
+    projection_func: Callable = projections.im2proj,
+    projection_kwargs: Dict = {},
     label: Optional[Union[Callable, str]] = None,
-    font: Optional[ImageFont.FreeTypeFont] = None
+    font: Optional[ImageFont.FreeTypeFont] = None,
+    begin_t: int = 0
 ) -> np.ndarray:
+    # Read timepoint
     log.info(f"Reading timepoint: {T}")
     if C:
         read_slice, shape = img.read_image(T=T, B=0, S=S, C=C)
@@ -38,12 +42,54 @@ def _process_timepoint(
 
     # Generate projection
     log.debug(f"Generating projection...")
-    proj = norm_func(read_slice, shape, **norm_kwargs)
+    proj = projection_func(read_slice, shape, **projection_kwargs)
 
     # Always cast to uint8
     proj = proj.astype(np.uint8)
 
-    # TODO: Add label generation
+    # Enforce projection dimensions
+    if len(proj.shape) not in (2, 3):
+        raise ValueError(
+            f"The array returned from provided projection function was {len(proj.shape)} dimensional. Expected 2 or 3."
+        )
+
+    # Add label if provided
+    if label:
+        # Call label function with parameters
+        if isinstance(label, Callable):
+            label = label(
+                metadata=metadata,
+                start_T=begin_t,
+                current_T=T,
+                shape=shape
+            )
+
+        # Always cast to a string
+        if not isinstance(label, str):
+            log.debug(f"Label for frame: {T} was provided as {label} with type: {type(label)}. Casting to string.")
+            label = str(label)
+
+        # Read projection as Image
+        im = Image.fromarray(proj)
+
+        # Get height and set intensity for text
+        if len(proj.shape) == 3:
+            height = proj.shape[1]
+            intensity = (255, 255, 255)
+
+        else:
+            height = proj.shape[0]
+            intensity = 255
+
+        # Label position is lower left
+        position = (10, height - 20)
+
+        # Attach label in lower left of image
+        drawer = ImageDraw.Draw(im)
+        drawer.text(position, label, intensity, font=font)
+
+        # Return to numpy array
+        proj = np.asarray(im).astype(np.uint8)
 
     log.info(f"Completed timepoint: {T}")
     return proj
@@ -53,8 +99,8 @@ def generate_movie(
     input_file: Union[str, Path],
     output_file: Union[str, Path],
     overwrite: bool = False,
-    norm_func: Callable = projections.im2proj,
-    norm_kwargs: Dict = {},
+    projection_func: Callable = projections.im2proj,
+    projection_kwargs: Dict = {},
     series_range: slice = slice(None, None, None),
     fps: int = 1,
     label: Optional[Union[Callable, str]] = None,
@@ -89,7 +135,8 @@ def generate_movie(
 
     # Read the font file if labels are desired
     if label:
-        font = ImageFont.truetype(font, 12)
+        font = ImageFont.truetype(str(font), 12)
+        metadata = img.read_meta()
     else:
         font = None
 
@@ -98,6 +145,11 @@ def generate_movie(
 
     # Generate iterator
     iterator = range(len_T)[series_range]
+
+    # Store beginning timepoint as it may be useful for labeling if desired
+    begin_t = iterator[0]
+
+    # Change iterator over to tqdm if desired
     if show_progress:
         iterator = tqdm(iterator)
 
@@ -106,13 +158,15 @@ def generate_movie(
         # Process timepoint
         processed = _process_timepoint(
             img=img,
+            metadata=metadata,
             T=i,
             S=S,
             C=C,
-            norm_func=norm_func,
-            norm_kwargs=norm_kwargs,
+            projection_func=projection_func,
+            projection_kwargs=projection_kwargs,
             label=label,
-            font=font
+            font=font,
+            begin_t=begin_t
         )
 
         # Append projection
--- timelapse_tools/projections.py
+++ timelapse_tools/projections.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from typing import List, Tuple
 
 import numpy as np
+from lxml.etree import _Element
 
 ###############################################################################
 
@@ -113,3 +115,22 @@ def im2proj_all_axes(
 
     # Combine upper and lower portions
     return np.vstack((upper, lower))
+
+
+def t_index_labeler(metadata: _Element, start_T: int, current_T: int, shape: List[Tuple[str, int]]) -> str:
+    return f"T: {current_T}"
+
+
+def t_plus_duration_labeler(metadata: _Element, start_T: int, current_T: int, shape: List[Tuple[str, int]]) -> str:
+    # Get timeline elements from metadata
+    timeline_elements = metadata.xpath(".//TimelineElement/Time")
+
+    # Get the first and current time point
+    begin = datetime.strptime(timeline_elements[start_T].text, "%Y-%m-%dT%H:%M:%S.%fZ")
+    current = datetime.strptime(timeline_elements[current_T].text, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    # Get duration
+    duration = str(current - begin)
+
+    # Return the duration from start
+    return f"Duration: {duration}"
