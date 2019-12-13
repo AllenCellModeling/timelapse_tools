aec34f00629e157f8151cfbd85c52596bd35b967
Jackson Brown <jmaxfieldbrown@gmail.com>
2019-12-13 14:49:00 -0800

    Add exception for invalid shape

--- timelapse_tools/exceptions.py
+++ timelapse_tools/exceptions.py
@@ -6,3 +6,16 @@
 
 class ConflictingArgumentsError(Exception):
     pass
+
+
+class InvalidShapeError(Exception):
+    def __init__(self, actual: int, expected: int):
+        self.actual = actual
+        self.expected = expected
+
+    def __str__(self):
+        return (
+            f"Invalid dimensions for this operation. "
+            f"Recieved array with {self.actual} dimensions. "
+            f"Expected array with {self.expected} dimensions. "
+        )
